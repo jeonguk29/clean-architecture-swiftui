ffc87bfc5082dc91b1c8c4577bfc8bd4b85b19d9
jeonguk29 <jeonguk29@naver.com>
2024-07-05 16:29:42 +0900

    CleanArchitecture 학습 프로젝트 분리

---
+++ TestCleanArchitecture /TestCleanArchitecture/TestCleanArchitecture/TestCleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCleanArchitectureApp.swift
+//  TestCleanArchitecture
+//
+//  Created by 정정욱 on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TestCleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
